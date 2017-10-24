1a99925c3762fe01224f08677b7a92201a61575d
DzungHg <hg.dzung@gmail.com>
2017-10-24 17:38:31 +0700

    Cải tiến tiếp
    Làm bế

---
+++ TinhGiaInBDO/BeDapBDO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinhGiaInBDO
+{
+    public class BeDapBDO : MayThanhPhamBaseBDO
+    {
+
+        public int TocDoTamGioGio { get; set; }
+        public int PhiNguyenVatLieuChuanBi { get; set; }
+    
+       
+    }
+}
